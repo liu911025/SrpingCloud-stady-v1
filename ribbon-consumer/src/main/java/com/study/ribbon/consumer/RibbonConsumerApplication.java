da3e48e973c78988059452fc82c23ada8f53aee2
ljt <liujiatai2016@163.com>
2020-07-23 17:34:55 +0800

    提交

--- ribbon-consumer/src/main/java/com/study/ribbon/consumer/RibbonConsumerApplication.java
+++ ribbon-consumer/src/main/java/com/study/ribbon/consumer/RibbonConsumerApplication.java
@@ -1,24 +1,24 @@
-package com.study.eureka.client;
+package com.study.ribbon.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
-public class EurekaClientApplication {
+public class RibbonConsumerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaClientApplication.class, args);
+        SpringApplication.run(RibbonConsumerApplication.class, args);
     }
-
 
     @LoadBalanced
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
+
 }
